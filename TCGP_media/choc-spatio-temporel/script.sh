dd678b53eda472385382cdfcfdcf3a114a3a41b2
Zamorak78 <66909737+Zamorak78@users.noreply.github.com>
2025-03-24 16:48:42 +0100

    Delete TCGP_media directory

--- TCGP_media/choc-spatio-temporel/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in {1..207}; do
-    curl https://image.pokemonpocket.fr/choc-spatio-temporel/$i.webp --output $i.webp
-done
--- TCGP_media/promo-a/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in {1..49}; do
-    curl https://image.pokemonpocket.fr/promo-a/$i.webp --output $i.webp
-done
