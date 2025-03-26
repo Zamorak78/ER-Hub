a1270173c7bdb74edbd469614080ff3e4d889958
Zamorak78 <66909737+Zamorak78@users.noreply.github.com>
2025-03-26 11:40:43 +0100

    Update promo-a.php

--- promo-a.php
+++ promo-a.php
@@ -5,7 +5,7 @@ $collections = [
         'title' => 'Promo-A',
         'sectionId' => 'section100',
         'totalCards' => 49,
-        'notGottenCards' => [13, 15, 18, 20, 21, 42]
+        'notGottenCards' => [10, 13, 15, 18, 20, 21, 42]
     ]
 ];
 
