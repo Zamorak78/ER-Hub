7d424a460fbe5f28a2d3ba09f707cca8d6951a0f
emilr <emilr.rouot@gmail.com>
2025-05-12 08:44:13 +0200

    daily boosters & trade

--- TCGP/secondary-accounts.php
+++ TCGP/secondary-accounts.php
@@ -42,7 +42,7 @@ $collections = [
             'sectionId' => 'section6',
             'totalCards' => 239,
             'notGottenCards' => [],
-            'displayCards' => [33, 167, 173, 211, 237]
+            'displayCards' => [33, 167, 167, 173, 211, 237]
         ],
     ],
     'Zamorak III - rsqake2' => [
@@ -86,7 +86,7 @@ $collections = [
             'sectionId' => 'section6',
             'totalCards' => 239,
             'notGottenCards' => [],
-            'displayCards' => [58, 122, 158, 159, 175, 175, 175, 210, 237, 238]
+            'displayCards' => [58, 122, 122, 158, 159, 160, 175, 175, 175, 210, 237, 238]
         ],
     ],
     'Zamorak IV - rsqake3' => [
@@ -102,7 +102,7 @@ $collections = [
             'sectionId' => 'section2',
             'totalCards' => 86,
             'notGottenCards' => [],
-            'displayCards' => [32, 71, 72, 72]
+            'displayCards' => [32, 72, 72]
         ],
         'choc-spatio-temporel' => [
             'title' => 'Choc Spatio-temporel',
@@ -146,7 +146,7 @@ $collections = [
             'sectionId' => 'section2',
             'totalCards' => 86,
             'notGottenCards' => [],
-            'displayCards' => [3, 69]
+            'displayCards' => [3]
         ],
         'choc-spatio-temporel' => [
             'title' => 'Choc Spatio-temporel',
