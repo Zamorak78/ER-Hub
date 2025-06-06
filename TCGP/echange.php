3b62a58e5dcbed4af375d4a458d5c68d92b7975c
emilr <emilr.rouot@gmail.com>
2025-06-06 21:26:23 +0200

    Lunala EX update

--- TCGP/echange.php
+++ TCGP/echange.php
@@ -41,7 +41,7 @@ $collections = [
         'sectionId' => 'section6',
         'totalCards' => 239,
         'notGottenCards' => [49],
-        'displayCards' => [12, 20, 22, 24, 33, 41, 49, 68, 87, 112, 143, 156, 158, 161, 163, 164, 166, 168, 173, 174, 178, 179]
+        'displayCards' => [12, 20, 22, 24, 33, 41, 49, 68, 112, 143, 156, 158, 161, 163, 164, 166, 168, 173, 174, 178, 179]
     ],
     'crise-interdimensionnelle' => [
         'title' => 'Crise Interdimensionnelle',
