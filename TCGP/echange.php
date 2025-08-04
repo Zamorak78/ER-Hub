4a12dde4449aa156d0b8cff79c7a5254355e3c83
emilr <emilr.rouot@gmail.com>
2025-08-04 14:08:31 +0200

    Sarmurai & Crabominable EX

--- TCGP/echange.php
+++ TCGP/echange.php
@@ -41,7 +41,7 @@ $collections = [
         'sectionId' => 'section6',
         'totalCards' => 239,
         'notGottenCards' => [],
-        'displayCards' => [22, 24, 41, 49, 112, 156, 158, 161, 163, 166, 178, 179]
+        'displayCards' => [24, 41, 112, 156, 158, 161, 163, 166, 178, 179]
     ],
     // 'crise-interdimensionnelle' => [
     //     'title' => 'Crise Interdimensionnelle',
