18c679d96ade8b2f04c53bfc5a6ab61322384a3e
emilr <emilr.rouot@gmail.com>
2025-08-10 21:27:14 +0200

    Leuphorie EX

--- TCGP/promo-a.php
+++ TCGP/promo-a.php
@@ -5,7 +5,7 @@ $collections = [
         'title' => 'Promo-A',
         'sectionId' => 'section100',
         'totalCards' => 100,
-        'notGottenCards' => [10, 13, 15, 18, 20, 21, 42, 52, 66, 74, 84, 93, 97, 98, 99, 100]
+        'notGottenCards' => [10, 13, 15, 18, 20, 21, 42, 52, 66, 74, 84, 93, 97, 99, 100]
     ]
 ];
 
