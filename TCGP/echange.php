5596e9e54cef9af4514814dbd9cef5c3bb8a9b84
emilr <emilr.rouot@gmail.com>
2025-09-11 09:25:55 +0200

    PorygonZ echange

--- TCGP/echange.php
+++ TCGP/echange.php
@@ -62,7 +62,7 @@ $collections = [
         'sectionId' => 'section9',
         'totalCards' => 241,
         'notGottenCards' => [82, 175],
-        'displayCards' => [34, 82, 112, 137, 163, 166, 167, 172, 175, 178, 179, 181, 183]
+        'displayCards' => [34, 82, 112, 163, 166, 167, 172, 175, 178, 179, 181, 183]
     ]
     // 'promo-a' => [
     //     'title' => 'Promo-A',
