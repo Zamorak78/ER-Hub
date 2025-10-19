aa8ac6a576d8460229bd8aa2b98b72e2f84e34bb
emilr <emilr.rouot@gmail.com>
2025-10-19 20:52:18 +0200

    promo-a update

--- TCGP/promo-a.php
+++ TCGP/promo-a.php
@@ -5,7 +5,7 @@ $collections = [
         'title' => 'Promo-A',
         'sectionId' => 'section100',
         'totalCards' => 117,
-        'notGottenCards' => [10, 13, 15, 18, 20, 21, 42, 52, 66, 74, 84, 93, 101, 109, 110, 116]
+        'notGottenCards' => [10, 13, 15, 18, 20, 21, 42, 52, 66, 74, 84, 93, 101, 109, 110]
     ]
 ];
 
